1e7bd633c8ca4c6a2fe4c512e67f659fd0787681
Aznaur <aliev.ak@phystech.edu>
2022-10-02 15:48:48 -0400

    initial implementation

--- src/graph.h
+++ src/graph.h
@@ -1,2 +1,30 @@
-#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+
+#define CELL_SIZE 2
+#define WIDTH 800
+#define HEIGHT 600
+#define A_WIDTH WIDTH / CELL_SIZE
+#define A_HEIGHT HEIGHT / CELL_SIZE
+
+class Game
+{
+    private:
+        bool** board;
+        bool** next_board;
+
+        sf::VertexArray pixels;
+
+    public:
+
+        Game(sf::RenderWindow& window);
+        Game(sf::RenderWindow& window, bool** inputArray);
+        ~Game();
+
+        void put_pixel(size_t px, size_t py, sf::Color cell);
+        void random_state();
+        void random_array();
+        int count_cells(int x, int y);
+        void next_state();
+        void image_from_array();
+        void update(sf::RenderWindow& window);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
+#include "graph.h"
 #include <iostream>
 
-int main(){
-    
-    return 0;
+#define DELTA_TIME 0.1
+
+int main(int argc, char** argv)
+{
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT ,32), "Window");
+    Game game (window);
+    sf::Clock timer; 
+    sf::Time delta_time = sf::seconds(DELTA_TIME);
+    game.random_state();
+
+    while (window.isOpen()) {
+        sf::Event appEvent;
+        while (window.pollEvent(appEvent)) {
+            switch(appEvent.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if (appEvent.key.code == sf::Keyboard::R) game.random_state();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        sf::Time time = timer.getElapsedTime(); 
+
+        if (time > delta_time) {
+            game.update(window);
+            game.next_state();
+            timer.restart();
+            window.display();
+            
+        }
+    }
+
+    return 0; 
 }
